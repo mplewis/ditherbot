4f26d38ef6eacee3b7fed2559e5e27a03cae593f
Matt Lewis <matt@mplewis.com>
2022-11-20 00:02:40 -0700

    feat: working resize, dither, custom palette

--- api/src/functions/ditherer/ditherer.ts
+++ api/src/functions/ditherer/ditherer.ts
@@ -1,7 +1,7 @@
 import type { APIGatewayEvent, Context } from 'aws-lambda'
 import fetch from 'cross-fetch'
 import * as iq from 'image-q'
-import { PNG } from 'pngjs'
+import jimp from 'jimp'
 import { z } from 'zod'
 
 import { logger } from 'src/lib/logger'
@@ -38,34 +38,38 @@ export const handler = async (event: APIGatewayEvent, context: Context) => {
 
   const rawBody = JSON.parse(event.body)
   const body = z.object({ image_url: z.string() }).parse(rawBody)
-  const resp = await fetch(body.image_url)
-  console.log({ resp })
 
-  const blob = await resp.blob()
-  const buffer = await blob.arrayBuffer()
-  console.log(`read bytes: ${buffer.byteLength}`)
-
-  const { data, width, height } = PNG.sync.read(Buffer.from(buffer))
-  const ipc = iq.utils.PointContainer.fromUint8Array(data, width, height)
-
+  const img = await jimp.read(body.image_url)
+  const width = img.getWidth()
+  const height = img.getHeight()
+  const newWidth = 48 // px
+  const newHeight = Math.round((newWidth / width) * height)
+  img.resize(newWidth, newHeight)
+  //
+  const ipc = iq.utils.PointContainer.fromUint8Array(
+    img.bitmap.data,
+    newWidth,
+    newHeight
+  )
   const imageQuantizer = new iq.image.ErrorDiffusionArray(
     new iq.distance.EuclideanBT709(),
-    iq.image.ErrorDiffusionArrayKernel.Jarvis
+    iq.image.ErrorDiffusionArrayKernel.Stucki
   )
-  const palette = new iq.utils.Palette()
-  palette.add(iq.utils.Point.createByRGBA(0, 0, 0, 255))
-  palette.add(iq.utils.Point.createByRGBA(255, 255, 255, 255))
-
+  // B&W palette
+  // const palette = new iq.utils.Palette()
+  // palette.add(iq.utils.Point.createByRGBA(0, 0, 0, 255))
+  // palette.add(iq.utils.Point.createByRGBA(255, 255, 255, 255))
+  const palette = iq.buildPaletteSync([ipc], { colors: 8 })
   const opc = imageQuantizer.quantizeSync(ipc, palette)
-  const opngObj = new PNG()
-  opngObj.data = Buffer.from(opc.toUint8Array())
-  opngObj.width = width
-  opngObj.height = height
-  const opng = PNG.sync.write(opngObj)
+  img.bitmap.data = Buffer.from(opc.toUint8Array())
+
+  console.log({ bytes: img.bitmap.data.length })
+
+  const buf = await img.getBufferAsync(jimp.MIME_PNG)
 
   return {
     statusCode: 200,
     headers: { 'Content-Type': 'image/png' },
-    body: opng,
+    body: buf,
   }
 }
